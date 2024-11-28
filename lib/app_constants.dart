e8f9858119c18a73e8a5c5a0b2cc52b7249e759f
anjali-luckimedia <anjali@luckimedia.com>
2024-11-28 16:02:39 +0530

    auth key change

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -8,5 +8,8 @@ abstract class FFAppConstants {
   //static const String baseUrl = 'https://aquamaticwp.elate-ecommerce.com/';///***Dev Old***
   static const String baseUrl = 'https://www.aquamatic.co.uk/';///***Live***
   //static const String basicAuth = 'Basic Y2tfZTZjMTYxYzRiMjEzZjM1YTRhZjVhNWRlMzkxZjcyNTUwYjA2ZmZhZjpjc18zNmRjMTllYzA3YjU5ODc2N2IzNjgzM2FkOGUyYTJkNDY5ZGVhMTlm';
-  static const String basicAuth = 'Basic Y2tfZGUyMjMyNDFiNGJjODYyMTA3YzEwZTBlMjVhMmFmODQ3N2VmYzQwMTpjc18wOGEwMDA1NDkwN2U2OGM1OGVlNzZkNWM4Y2Q0YTQwNGJiOTA0NTFk';
+
+  static const String basicAuth = 'Basic Y2tfNThjYmZjNTJkMWY3MDBjODM4ZTFjNjBkMjRiZTBjMTA4YTA5ZTBkNDpjc19kNjA4ZDk4Yzc1N2JmNWEyYmQ1MWZiYmFkZGQ3ODA4ZDZjODdmNjY4';
+
+  //static const String basicAuth = 'Basic Y2tfZGUyMjMyNDFiNGJjODYyMTA3YzEwZTBlMjVhMmFmODQ3N2VmYzQwMTpjc18wOGEwMDA1NDkwN2U2OGM1OGVlNzZkNWM4Y2Q0YTQwNGJiOTA0NTFk';
 }
